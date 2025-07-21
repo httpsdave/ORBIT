8a6871af9dc4c55a6acd6dbb5abb6b6cf7779666
httpsdave <davedominic912@gmail.com>
2025-07-21 11:50:26 +0800

    LSPU logo on sidebar footer

--- OSASvueinertia/resources/js/Components/Layout/Sidebar/SidebarFooter.vue
+++ OSASvueinertia/resources/js/Components/Layout/Sidebar/SidebarFooter.vue
@@ -15,13 +15,16 @@ const props = defineProps({
 </script>
 
 <template>
-  <!-- Empty footer container that will remain responsive -->
-  <div class="mt-auto p-4 border-t border-gray-200">
-    <div class="flex w-full overflow-hidden shadow-md">
+  <!-- Footer container with colored banner and logo -->
+  <div class="mt-auto p-4 border-t border-gray-200 flex flex-col items-center">
+    <!-- Colored banner at the top of the footer -->
+    <div class="flex w-full overflow-hidden shadow-md mb-4">
       <div class="w-1/4 h-1.5 bg-blue-600 " style="animation-delay: 0.2s;"></div>
       <div class="w-1/4 h-1.5 bg-green-500 " style="animation-delay: 0.4s;"></div>
       <div class="w-1/4 h-1.5 bg-amber-500 " style="animation-delay: 0.6s;"></div>
       <div class="w-1/4 h-1.5 bg-red-500 " style="animation-delay: 0.8s;"></div>
     </div>
+    <!-- LSPU logo centered below the colored banner -->
+    <img src="/images/lspu_logo_better.png" alt="LSPU Logo" class="h-12 w-auto" />
   </div>
 </template>
