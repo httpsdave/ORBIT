7885f5bdbedb1d6fa3ba0f1d3795c186c3d2946b
httpsdave <davedominic912@gmail.com>
2025-07-26 21:57:47 +0800

    changed Document uploaded --> Signed Document

--- OSASvueinertia/resources/js/Components/ApplicationsTable.vue
+++ OSASvueinertia/resources/js/Components/ApplicationsTable.vue
@@ -457,7 +457,7 @@ const viewPdf = (app) => {
             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
             </svg>
-            Document uploaded
+            Signed Document
           </span>
           <span v-else class="text-gray-500 flex items-center gap-1">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
@@ -579,7 +579,7 @@ const viewPdf = (app) => {
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
-                Document uploaded
+                Signed Document
               </span>
               <span v-else class="text-gray-500 flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
