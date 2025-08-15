d7940f4399fe0280b436c2d7108d1d03f3c41f84
httpsdave <davedominic912@gmail.com>
2025-08-15 22:14:17 +0800

    fixed spacing between recommending approval and approved/disapproved 001

--- OSASvueinertia/resources/views/pdfs/organization_application.blade.php
+++ OSASvueinertia/resources/views/pdfs/organization_application.blade.php
@@ -285,16 +285,16 @@
     <p style="margin-bottom: 0;"><strong>Recommending Approval:</strong></p>
     </div>
     <div class="signature center-align" style="margin-top: 0;">
-        <p style="margin-bottom: 0;"><strong><span class="signature-line">{{ $application->coordinator_name ?? 'N/A' }}</span></strong></p>
-        <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Coordinator, Student Organization Unit</span></p>
+    <p style="margin-bottom: 0; margin-top: -10px;"><strong><span class="signature-line">{{ $application->coordinator_name ?? 'N/A' }}</span></strong></p>
+    <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Coordinator, Student Organization Unit</span></p>
     </div>
 
     <div class="section center-align last-section" style="margin-bottom: 0;">
     <p style="margin-bottom: 0;"><strong>Approved/Disapproved:</strong></p>
     </div>
     <div class="signature center-align last-signature" style="margin-top: 0;">
-        <p style="margin-bottom: 0;"><strong><span class="signature-line">{{ $application->director_name ?? 'N/A' }}</span></strong></p>
-        <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Director/Chairperson, Office of Student Affairs and Services</span></p>
+    <p style="margin-bottom: 0; margin-top: -6px;"><strong><span class="signature-line">{{ $application->director_name ?? 'N/A' }}</span></strong></p>
+    <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Director/Chairperson, Office of Student Affairs and Services</span></p>
     </div>
 
     <div class="footer">
