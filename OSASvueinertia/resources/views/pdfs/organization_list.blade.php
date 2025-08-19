5f8fdbcdae2913b097a751a313a3cc52ead2c188
httpsdave <davedominic912@gmail.com>
2025-08-19 23:23:15 +0800

    99% 005

--- OSASvueinertia/resources/views/pdfs/organization_list.blade.php
+++ OSASvueinertia/resources/views/pdfs/organization_list.blade.php
@@ -552,8 +552,8 @@
         </table>
         <div class="dean-signature center-align" style="width: 50% !important; margin-top: 3px; text-align: center !important;">
             <p style="margin-bottom: 0; font-weight: bold !important; margin-left: 10px !important; text-align: left !important; margin-top: -5px;">Noted:</p>
-            <p style="margin-bottom: 0; text-align: center !important;"><span class="date-signature-line" style="display: inline-block; min-width: 180px;">{{ $application->dean_name ?? '' }}</span></p>
-            <p style="margin-top: 2px; font-weight: normal; margin-left: 0 !important; text-align: center !important;">Dean/Assoc. Dean of College</p>
+            <p style="margin-bottom: 0; text-align: center !important; margin-top: -3px;"><span class="date-signature-line" style="display: inline-block; min-width: 180px;">{{ $application->dean_name ?? '' }}</span></p>
+            <p style="margin-top: 3px; font-weight: normal; margin-left: 0 !important; text-align: center !important;">Dean/Assoc. Dean of College</p>
         </div>
 
         <div class="recommendation" style="margin-top:-10px; text-align: center;">
