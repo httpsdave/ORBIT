a814f5b75935968e7b9171f4f82b729890d26bdb
httpsdave <davedominic912@gmail.com>
2025-08-20 22:45:39 +0800

    fixing content and spacing 006

--- OSASvueinertia/resources/views/pdfs/organization_certification.blade.php
+++ OSASvueinertia/resources/views/pdfs/organization_certification.blade.php
@@ -377,11 +377,9 @@
                 <div style="text-align: center; font-size: 11pt; margin-top: -5px;">
                     (course, year and section)
                 </div>
-            </div> from the College of <span class="signature-line" style="min-width:200px; border-bottom: 1px solid black; display: inline-block;">{{ $certification->college ?? ' ' }}</span>
+            </div> from the College of <span class="signature-line" style="min-width:385px; border-bottom: 1px solid black; display: inline-block;">{{ $certification->college ?? ' ' }}</span> is a bonafide LSPU Student, not
             <br><br>
             <div class="cert-content" style="margin-top: -20px;">
-            <br>
-            is a bonafide LSPU Student, not
             <br><br>
             under academic probation, not under disciplinary probation, and the elected/appointed
             <br><br>
