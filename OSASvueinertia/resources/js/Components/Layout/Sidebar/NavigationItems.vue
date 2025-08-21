a09166d685a3bd731cd8a899d68ef0742479c4b5
httpsdave <davedominic912@gmail.com>
2025-08-21 23:18:45 +0800

    changed applications link in user side sidebarlayout to archives

--- OSASvueinertia/resources/js/Components/Layout/Sidebar/NavigationItems.vue
+++ OSASvueinertia/resources/js/Components/Layout/Sidebar/NavigationItems.vue
@@ -56,9 +56,9 @@ const userNavItems = [
     icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
   },
   {
-    name: 'Applications',
-    route: 'applications.index',
-    icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
+    name: 'Archive',
+    route: 'archive.index',
+    icon: 'M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4'
   },
   {
     name: 'Colleges',
