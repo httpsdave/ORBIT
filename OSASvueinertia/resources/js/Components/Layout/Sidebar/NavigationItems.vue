022b3ab089687bd9d77d10f3f48d28159564ec47
httpsdave <davedominic912@gmail.com>
2025-08-21 23:21:29 +0800

    added archive management for admin in sidebarlayout

--- OSASvueinertia/resources/js/Components/Layout/Sidebar/NavigationItems.vue
+++ OSASvueinertia/resources/js/Components/Layout/Sidebar/NavigationItems.vue
@@ -131,6 +131,19 @@ const handleActionClick = (event) => {
           <div class="mt-4">
             <NavigationItem 
               :item="{
+                name: 'Archive Management',
+                route: 'archive.index',
+                icon: 'M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4'
+              }" 
+              :sidebar-expanded="sidebarExpanded" 
+              :showing-sidebar="showingSidebar" 
+            />
+          </div>
+          
+          <!-- Calendar link after Archive Management -->
+          <div class="mt-4">
+            <NavigationItem 
+              :item="{
                 name: 'Calendar',
                 route: 'calendar',
                 icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
