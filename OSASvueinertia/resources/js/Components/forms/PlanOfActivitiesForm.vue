f54df6bcc7fab55cfa490d05563ef91b6b173321
httpsdave <davedominic912@gmail.com>
2025-08-22 12:35:12 +0800

    fixing validation for POA

--- OSASvueinertia/resources/js/Components/forms/PlanOfActivitiesForm.vue
+++ OSASvueinertia/resources/js/Components/forms/PlanOfActivitiesForm.vue
@@ -441,23 +441,37 @@ const validateForm = () => {
       return tmp.textContent || tmp.innerText || '';
     };
 
+    // Character limit validation
+    const MAX_ACTIVITY_CHAR = 144;
     if (!activity.objective || !stripHtml(activity.objective).trim()) {
       errors.value.activities[index].objective = 'Objective is required';
+      isValid = false;
+    } else if (stripHtml(activity.objective).length > MAX_ACTIVITY_CHAR) {
+      errors.value.activities[index].objective = `Objective must be ${MAX_ACTIVITY_CHAR} characters or less.`;
       isValid = false;
     }
 
     if (!activity.name || !stripHtml(activity.name).trim()) {
       errors.value.activities[index].name = 'Activity name is required';
       isValid = false;
+    } else if (stripHtml(activity.name).length > MAX_ACTIVITY_CHAR) {
+      errors.value.activities[index].name = `Activity name must be ${MAX_ACTIVITY_CHAR} characters or less.`;
+      isValid = false;
     }
 
     if (!activity.description || !stripHtml(activity.description).trim()) {
       errors.value.activities[index].description = 'Description is required';
       isValid = false;
+    } else if (stripHtml(activity.description).length > MAX_ACTIVITY_CHAR) {
+      errors.value.activities[index].description = `Description must be ${MAX_ACTIVITY_CHAR} characters or less.`;
+      isValid = false;
     }
 
     if (!activity.persons_involved || !stripHtml(activity.persons_involved).trim()) {
       errors.value.activities[index].persons_involved = 'Persons involved is required';
+      isValid = false;
+    } else if (stripHtml(activity.persons_involved).length > MAX_ACTIVITY_CHAR) {
+      errors.value.activities[index].persons_involved = `Persons involved must be ${MAX_ACTIVITY_CHAR} characters or less.`;
       isValid = false;
     }
 
