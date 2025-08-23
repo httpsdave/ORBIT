9618a4092ab2d091c26312d7687df125632722c7
httpsdave <davedominic912@gmail.com>
2025-08-23 19:27:13 +0800

    clear saved data icon now responsive to themes

--- OSASvueinertia/resources/js/Pages/OrganizationApplications/Index.vue
+++ OSASvueinertia/resources/js/Pages/OrganizationApplications/Index.vue
@@ -562,11 +562,11 @@ const confirmClearData = () => {
             </button>
             <button
               @click="openClearDataModal"
-              class="inline-flex items-center justify-center p-2 bg-transparent hover:bg-red-50 dark:hover:bg-red-900/20 text-gray-600 dark:text-gray-400 hover:text-red-600 dark:hover:text-red-400 rounded-lg shadow-sm transition duration-200 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 dark:focus:ring-offset-gray-900"
+              class="inline-flex items-center justify-center p-2 bg-transparent hover:bg-red-50 dark:hover:bg-red-900/20 text-gray-700 dark:text-gray-300 hover:text-red-600 dark:hover:text-red-400 rounded-lg shadow-sm transition duration-200 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 dark:focus:ring-offset-gray-900"
               aria-label="Clear Saved Form Data"
               title="Clear all saved form data that auto-fills when creating applications"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3">
+              <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="currentColor">
                 <path d="M280-720v520-520Zm170 600H280q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v172q-17-5-39.5-8.5T680-560v-160H280v520h132q6 21 16 41.5t22 38.5Zm-90-160h40q0-63 20-103.5l20-40.5v-216h-80v360Zm160-230q17-11 38.5-22t41.5-16v-92h-80v130ZM680-80q-83 0-141.5-58.5T480-280q0-83 58.5-141.5T680-480q83 0 141.5 58.5T880-280q0 83-58.5 141.5T680-80Zm66-106 28-28-74-74v-112h-40v128l86 86Z"/>
               </svg>
             </button>
