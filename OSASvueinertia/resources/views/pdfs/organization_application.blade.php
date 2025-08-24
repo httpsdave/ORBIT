cf2f376e64f8d242529b190490153ff89ac0cbc8
httpsdave <davedominic912@gmail.com>
2025-08-24 21:42:43 +0800

    RESTORED 001 blade

--- OSASvueinertia/resources/views/pdfs/organization_application.blade.php
+++ OSASvueinertia/resources/views/pdfs/organization_application.blade.php
@@ -289,7 +289,7 @@
     <p style="margin-bottom: 0;"><strong>Recommending Approval:</strong></p>
     </div>
     <div class="signature center-align" style="margin-top: 0;">
-    <p style="margin-bottom: 0; margin-top: -10px;"><strong><span class="signature-line">{{ $application->coordinator_name ?? 'N/A' }}</span></strong></p>
+    <p style="margin-bottom: 0; margin-top: -10px;"><strong><span class="signature-line" style="min-width:270px;">{{ $application->coordinator_name ?? 'N/A' }}</span></strong></p>
     <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Coordinator, Student Organization Unit</span></p>
     <div style="height: 5px;"></div>
     </div>
@@ -298,7 +298,7 @@
     <p style="margin-bottom: 0;"><strong>Approved/Disapproved:</strong></p>
     </div>
     <div class="signature center-align last-signature" style="margin-top: 0;">
-    <p style="margin-bottom: 0; margin-top: -6px;"><strong><span class="signature-line">{{ $application->director_name ?? 'N/A' }}</span></strong></p>
+    <p style="margin-bottom: 0; margin-top: -6px;"><strong><span class="signature-line" style="min-width:380px;">{{ $application->director_name ?? 'N/A' }}</span></strong></p>
     <p style="margin-top: 0; margin-bottom: 0;"><span class="title-text long-title">Director/Chairperson, Office of Student Affairs and Services</span></p>
     </div>
 
