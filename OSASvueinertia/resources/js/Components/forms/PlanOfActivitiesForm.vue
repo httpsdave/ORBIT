5824798bb3d542a91aca7c155f2e194993be1ed6
httpsdave <davedominic912@gmail.com>
2025-08-25 17:30:18 +0800

    fixed layout for Form Details in PlanOfActivitiesForm.vue

--- OSASvueinertia/resources/js/Components/forms/PlanOfActivitiesForm.vue
+++ OSASvueinertia/resources/js/Components/forms/PlanOfActivitiesForm.vue
@@ -886,23 +886,14 @@ nextTick(() => {
       <h3 class="text-lg font-bold mb-4">Form Details</h3>
       
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <!-- Left Column -->
         <div>
           <label class="block font-bold">Organization Name</label>
           <input v-model="form.organization_name" class="border p-2 w-full" required>
           <p v-if="errors.organization_name" class="text-red-500 text-sm mt-1">{{ errors.organization_name }}</p>
         </div>
 
-        <div>
-          <label class="block font-bold">Semester</label>
-          <select v-model="form.semester" class="border p-2 w-full" required>
-            <option value="">Select Semester</option>
-            <option value="1st">1st</option>
-            <option value="2nd">2nd</option>
-            <option value="Inter">Inter semester</option>
-          </select>
-          <p v-if="errors.semester" class="text-red-500 text-sm mt-1">{{ errors.semester }}</p>
-        </div>
-
+        <!-- Right Column -->
         <div class="flex items-end space-x-2">
           <div>
             <label class="block font-bold">Academic Year</label>
@@ -932,38 +923,62 @@ nextTick(() => {
           </div>
         </div>
 
+        <!-- Left Column -->
+        <div>
+          <label class="block font-bold">Semester</label>
+          <select v-model="form.semester" class="border p-2 w-full" required>
+            <option value="">Select Semester</option>
+            <option value="1st">1st</option>
+            <option value="2nd">2nd</option>
+            <option value="Inter">Inter semester</option>
+          </select>
+          <p v-if="errors.semester" class="text-red-500 text-sm mt-1">{{ errors.semester }}</p>
+        </div>
+
+        <!-- Right Column -->
+        <div>
+          <label class="block font-bold">Coordinator Name</label>
+          <input v-model="form.coordinator_name" class="border p-2 w-full bg-gray-200 text-gray-500 select-none pointer-events-none" readonly tabindex="-1" style="user-select: none; -webkit-user-select: none;">
+        </div>
+
+        <!-- Left Column -->
         <div>
           <label class="block font-bold">President Name</label>
           <input v-model="form.president_name" class="border p-2 w-full" required>
           <p v-if="errors.president_name" class="text-red-500 text-sm mt-1">{{ errors.president_name }}</p>
         </div>
 
+        <!-- Right Column -->
+        <div>
+          <label class="block font-bold">Director Name</label>
+          <input v-model="form.director_name" class="border p-2 w-full bg-gray-200 text-gray-500 select-none pointer-events-none" readonly tabindex="-1" style="user-select: none; -webkit-user-select: none;">
+        </div>
+
+        <!-- Left Column -->
         <div>
           <label class="block font-bold">Secretary Name</label>
           <input v-model="form.secretary_name" class="border p-2 w-full" required>
           <p v-if="errors.secretary_name" class="text-red-500 text-sm mt-1">{{ errors.secretary_name }}</p>
         </div>
 
+        <!-- Empty slot for alignment -->
+        <div></div>
+
+        <!-- Left Column -->
         <div>
           <label class="block font-bold">Adviser Name</label>
           <input v-model="form.adviser_name" class="border p-2 w-full" required>
           <p v-if="errors.adviser_name" class="text-red-500 text-sm mt-1">{{ errors.adviser_name }}</p>
         </div>
 
+        <!-- Empty slot for alignment -->
+        <div></div>
+
+        <!-- Left Column -->
         <div>
           <label class="block font-bold">Dean Name</label>
           <input v-model="form.dean_name" class="border p-2 w-full" required>
           <p v-if="errors.dean_name" class="text-red-500 text-sm mt-1">{{ errors.dean_name }}</p>
-        </div>
-
-        <div>
-          <label class="block font-bold">Coordinator Name</label>
-          <input v-model="form.coordinator_name" class="border p-2 w-full bg-gray-200 text-gray-500 select-none pointer-events-none" readonly tabindex="-1" style="user-select: none; -webkit-user-select: none;">
-        </div>
-
-        <div>
-          <label class="block font-bold">Director Name</label>
-          <input v-model="form.director_name" class="border p-2 w-full bg-gray-200 text-gray-500 select-none pointer-events-none" readonly tabindex="-1" style="user-select: none; -webkit-user-select: none;">
         </div>
       </div>
 
