a5e3d340049868aa1b87b05c921ba33fbcd7e7c8
httpsdave <davedominic912@gmail.com>
2025-08-26 17:47:36 +0800

    fixing signature lines for member info 005 form

--- OSASvueinertia/resources/js/Components/forms/ListOfMembersForm.vue
+++ OSASvueinertia/resources/js/Components/forms/ListOfMembersForm.vue
@@ -426,13 +426,13 @@ const submit = () => {
                   <span v-else class="photo-box-text text-center leading-tight">1 x 1<br>PICTURE</span>
                 </div>
                 <div class="member-info flex-1 flex flex-col justify-center text-center">
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto">
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto">
                     <span class="filled-text">{{ form.members[startIndex + (rowIndex - 1)].student_name }}</span>
                   </div>
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto">
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto">
                     <span class="filled-text">{{ form.members[startIndex + (rowIndex - 1)].student_number }}</span>
                   </div>
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto">
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto">
                     <span class="filled-text">{{ form.members[startIndex + (rowIndex - 1)].course_year_section }}</span>
                   </div>
                 </div>
@@ -442,9 +442,9 @@ const submit = () => {
                   <span class="photo-box-text text-center leading-tight">1 x 1<br>PICTURE</span>
                 </div>
                 <div class="member-info flex-1 flex flex-col justify-center text-center">
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
-                  <div class="member-line mb-1 min-h-[20px] py-1 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
+                  <div class="member-line mb-0 min-h-[18px] py-0 border-b border-black w-4/5 mx-auto"><span class="filled-text"></span></div>
                 </div>
               </div>
             </div>
