2b04d2d1498eb62180e360bcead4e5a908b93ebf
httpsdave <davedominic912@gmail.com>
2025-09-09 18:44:22 +0800

    removed continue to selected form button to reduce redundancy

--- OSASvueinertia/resources/js/Components/FormSelector.vue
+++ OSASvueinertia/resources/js/Components/FormSelector.vue
@@ -115,17 +115,6 @@ onUnmounted(() => {
           </div>
         </div>
       </div>
-
-      <!-- Submission Button (only visible when a form is selected) -->
-      <div v-if="selectedForm" class="mt-4 sm:mt-6">
-        <button
-          type="button"
-          @click="emit('form-selected', selectedForm)"
-          class="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-blue-500 transition-colors duration-150"
-        >
-          Continue to Selected Form
-        </button>
-      </div>
     </div>
   </div>
 </template>
