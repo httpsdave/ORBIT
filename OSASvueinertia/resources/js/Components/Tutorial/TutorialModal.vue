8936ab603ab12c1b0966e51f06e56ed517f7d606
httpsdave <davedominic912@gmail.com>
2025-10-25 14:28:08 +0800

    final touches for tutorial modal

--- OSASvueinertia/resources/js/Components/Tutorial/TutorialModal.vue
+++ OSASvueinertia/resources/js/Components/Tutorial/TutorialModal.vue
@@ -43,8 +43,8 @@ const userSteps = [
         highlight: 'applications'
     },
     {
-        title: 'Manage Events 📅',
-        description: 'View and manage your organization\'s events through the Calendar. Stay updated on important dates and activities.',
+        title: 'View Events 📅',
+        description: 'View your organization\'s events through the Calendar. Stay updated on important dates and upcoming activities.',
         iconClass: 'text-orange-500',
         iconPath: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z',
         highlight: 'calendar'
