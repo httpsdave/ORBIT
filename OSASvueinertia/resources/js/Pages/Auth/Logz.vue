9ee014731f9da3b5d8c4c8b8eb2733bd4c47cdbf
httpsdave <davedominic912@gmail.com>
2025-11-08 21:22:37 +0800

    fixed remember me and forgot password button spacing and font size

--- OSASvueinertia/resources/js/Pages/Auth/Logz.vue
+++ OSASvueinertia/resources/js/Pages/Auth/Logz.vue
@@ -482,7 +482,7 @@ onBeforeUnmount(() => {
                     </div>
 
                     <!-- Remember me and forgot password -->
-                    <div class="flex flex-row items-center justify-between space-x-2 min-h-[2.5rem]">
+                    <div class="flex flex-row items-center justify-between space-x-3 sm:space-x-4 min-h-[2.5rem]">
                         <label class="flex items-center group cursor-pointer flex-shrink-0">
                             <Checkbox 
                                 name="remember" 
@@ -490,7 +490,7 @@ onBeforeUnmount(() => {
                                 class="text-blue-500 rounded focus:ring-blue-500 border-gray-400" 
                             />
                             <span 
-                                class="ml-2 text-xs group-hover:text-gray-200 transition-colors duration-300" 
+                                class="ml-2 sm:ml-3 text-xs sm:text-sm group-hover:text-gray-200 transition-colors duration-300" 
                                 :class="isDarkMode ? 'text-gray-400' : 'text-gray-300'"
                             >
                                 Remember me
@@ -500,7 +500,7 @@ onBeforeUnmount(() => {
                         <Link
                             v-if="canResetPassword"
                             :href="route('password.request')"
-                            class="text-xs hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 rounded-md transition duration-300 whitespace-nowrap flex-shrink-0"
+                            class="text-xs sm:text-sm hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 rounded-md transition duration-300 whitespace-nowrap flex-shrink-0"
                             :class="isDarkMode ? 'text-gray-400' : 'text-gray-300'"
                         >
                             Forgot Password?
