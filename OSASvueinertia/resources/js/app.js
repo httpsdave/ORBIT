f1dbd8ceca71f72d7f3a2f3fd36ab4a0b6c305dc
httpsdave <davedominic912@gmail.com>
2025-11-09 20:18:13 +0800

    maxing out on performance LIGHTHOUSE

--- OSASvueinertia/resources/js/app.js
+++ OSASvueinertia/resources/js/app.js
@@ -4,10 +4,11 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
+import { createApp, h, defineAsyncComponent } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { useTheme } from '@/Composables/useTheme';
-import LoadingBar from '@/Components/LoadingBar.vue';
+
+const LoadingBar = defineAsyncComponent(() => import('@/Components/LoadingBar.vue'));
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
