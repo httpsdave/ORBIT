4cb5785076ba6656f42c90e7ea61d0d8f333d89b
httpsdave <davedominic912@gmail.com>
2025-11-20 15:49:22 +0800

    added admin to archive report viewing:

--- OSASvueinertia/resources/js/Pages/Applications/Reports.vue
+++ OSASvueinertia/resources/js/Pages/Applications/Reports.vue
@@ -286,7 +286,16 @@ const getStatusBadgeClass = (status) => {
 }
 
 const goBack = () => {
-  router.visit('/applications')
+  // If archived, check if admin or user and route accordingly
+  if (props.isArchived) {
+    if (props.isAdmin) {
+      router.visit('/admin/archive')
+    } else {
+      router.visit('/archive')
+    }
+  } else {
+    router.visit('/applications')
+  }
 }
 
 // File handling functions
@@ -792,7 +801,7 @@ watch(showStatusModal, (val) => {
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
           </svg>
-          Back to Applications
+          {{ isArchived ? 'Back to Archive' : 'Back to Applications' }}
         </button>
         
         <!-- Archived Badge -->
